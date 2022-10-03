f805ab3321133d60df46a227fb67f5ede70aff88
Paola Barrios <pbarrios0795@gmail.com>
2022-10-03 19:28:47 -0300

    octava subida

---
+++ codigo.js
@@ -0,0 +1,9 @@
+
+    function guardarConsulta()
+    {
+        var mail=document.getElementById("emailUser").value;
+        var asunt=document.getElementById("asuntoTxt").value;
+        var consult=document.getElementById("ConsultaTxt").value;
+    }
+
+
